5c25dee09423eec54671eca653ca7e28f2cdddd9
WorthyD <danieljworthy@gmail.com>
2025-09-28 20:12:06 -0500

    chore: prepping for pager

--- libs/shared/ui/data-viewer/src/lib/data-viewer/data-viewer.component.ts
+++ libs/shared/ui/data-viewer/src/lib/data-viewer/data-viewer.component.ts
@@ -1,35 +1,79 @@
-import { ChangeDetectionStrategy, Component, ContentChild, input, TemplateRef, ViewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  ContentChild,
+  input,
+  Signal,
+  signal,
+  TemplateRef,
+  ViewChild,
+  WritableSignal
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RenderedViewDynamicCompDirective } from './data-viewer.directive';
 import { DataViewerConfig } from './data-viewer.config';
 // import {} from '@cr
-
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { DataPage } from '../models/data-page.interface';
 @Component({
   selector: 'lib-data-viewer',
-  imports: [CommonModule],
+  imports: [CommonModule, MatPaginatorModule],
   template: `
     <h1>Data Viewer</h1>
-    <!-- RenderedViewDynamicCompDirective -->
-    <!-- <ng-template libDynamicComp></ng-template> -->
-    <div class="flex flex-wrap justify-around gap-4">
-      <!-- Temp Card List -->
-      <!-- <ng-container *ngFor="let item of dataViewerConfig().data; trackBy: trackByItem"> -->
 
-      @for (item of dataViewerConfig().data; track item) {
-        <ng-container *ngTemplateOutlet="projectedTemplate; context: { data: item }"></ng-container>
+    @switch (currentView) {
+      @case ('card') {
+        <div class="flex flex-wrap justify-around gap-4">
+          <!-- Temp Card List -->
+          @for (item of renderedData(); track trackByItem($index, item)) {
+            <ng-container *ngTemplateOutlet="projectedTemplate; context: { data: item }"></ng-container>
+          }
+        </div>
       }
-    </div>
+      @case ('table') {
+        <p>Table View</p>
+      }
+    }
+    <mat-paginator
+      (page)="setPage($event)"
+      [pageSizeOptions]="[10, 25, 50, 100]"
+      [pageSize]="pagerData().size"
+      [length]="curatedData().length"
+    >
+    </mat-paginator>
   `,
   styles: [``],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DataViewerComponent<T> {
+export class DataViewerComponent<T extends { id?: unknown }> {
   @ViewChild(RenderedViewDynamicCompDirective, { static: true }) libDynamicComp!: RenderedViewDynamicCompDirective;
   @ContentChild('cardTemplate') projectedTemplate: TemplateRef<{ data: T }> = null!;
 
+  constructor() {}
+
+  currentView: 'card' | 'table' = 'card';
+
   dataViewerConfig = input.required<DataViewerConfig<T>>();
 
+  pagerData: WritableSignal<DataPage> = signal({ index: 0, size: 25 });
+  curatedData: Signal<T[]> = computed(() => this.dataViewerConfig().data);
+
+  //TODO: include filters
+  itemCount = computed(() => this.curatedData.length);
+
+  renderedData: Signal<T[]> = computed(() => {
+    const data = this.dataViewerConfig().data;
+    const page = this.pagerData();
+    console.log('data', data);
+    return data.slice(page.index * page.size, page.index * page.size + page.size);
+  });
+
+  setPage(event: PageEvent) {
+    this.pagerData.set({ index: event.pageIndex, size: event.pageSize });
+  }
+
   trackByItem(index: number, item: T): any {
-    return item;
+    return item.id ?? index;
   }
 }
