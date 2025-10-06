79bbc4e467136c0c2741d91d38fc623106f11c2e
WorthyD <danieljworthy@gmail.com>
2025-10-06 17:31:03 -0500

    stuff

--- libs/shared/ui/data-viewer/src/lib/data-viewer/data-viewer.component.ts
+++ libs/shared/ui/data-viewer/src/lib/data-viewer/data-viewer.component.ts
@@ -1,4 +1,5 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   computed,
@@ -14,30 +15,53 @@ import { CommonModule } from '@angular/common';
 import { RenderedViewDynamicCompDirective } from './data-viewer.directive';
 import { DataViewerConfig } from './data-viewer.config';
 // import {} from '@cr
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { DataPage } from '../models/data-page.interface';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'lib-data-viewer',
-  imports: [CommonModule, MatPaginatorModule],
+  imports: [
+    CommonModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSortModule
+  ],
   template: `
     <h1>Data Viewer</h1>
-
-    @switch (currentView) {
+    <mat-button-toggle-group [formControl]="currentView" aria-label="Font Style">
+      <mat-button-toggle value="card">Card</mat-button-toggle>
+      <mat-button-toggle value="table">Table</mat-button-toggle>
+    </mat-button-toggle-group>
+    {{ currentView.value }}
+    @switch (currentView.value) {
       @case ('card') {
         <div class="flex flex-wrap justify-around gap-4">
           <!-- Temp Card List -->
-          @for (item of renderedData(); track trackByItem($index, item)) {
+          @for (item of dataSource().filteredData; track trackByItem($index, item)) {
             <ng-container *ngTemplateOutlet="projectedTemplate; context: { data: item }"></ng-container>
           }
         </div>
       }
       @case ('table') {
-        <p>Table View</p>
+        <table mat-table [dataSource]="dataSource()" matSort>
+          <ng-container matColumnDef="id">
+            <th mat-header-cell *matHeaderCellDef mat-sort-header>ID</th>
+            <td mat-cell *matCellDef="let row">{{ row.id }}</td>
+          </ng-container>
+          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+          <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
+        </table>
       }
     }
     <mat-paginator
       (page)="setPage($event)"
-      [pageSizeOptions]="[10, 25, 50, 100]"
+      [pageSizeOptions]="[1, 10, 25, 50, 100]"
       [pageSize]="pagerData().size"
       [length]="curatedData().length"
     >
@@ -46,18 +70,30 @@ import { DataPage } from '../models/data-page.interface';
   styles: [``],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DataViewerComponent<T extends { id?: unknown }> {
+export class DataViewerComponent<T extends { id?: unknown }> implements AfterViewInit {
   @ViewChild(RenderedViewDynamicCompDirective, { static: true }) libDynamicComp!: RenderedViewDynamicCompDirective;
   @ContentChild('cardTemplate') projectedTemplate: TemplateRef<{ data: T }> = null!;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor() {
+    //this.dataSource = new MatTableDataSource<T>([]);
+    //this.dataSource = new MatTableDataSource<T>(this.dataViewerConfig().data ?? []);
+  }
+  ngAfterViewInit() {
+    this.dataSource().paginator = this.paginator;
+    this.dataSource().sort = this.sort;
+  }
 
-  constructor() {}
+  currentView = new FormControl('card');
+  displayedColumns: string[] = ['id'];
 
-  currentView: 'card' | 'table' = 'card';
+  //currentView: 'card' | 'table' = 'card';
 
   dataViewerConfig = input.required<DataViewerConfig<T>>();
 
   pagerData: WritableSignal<DataPage> = signal({ index: 0, size: 25 });
   curatedData: Signal<T[]> = computed(() => this.dataViewerConfig().data);
+  dataSource: Signal<MatTableDataSource<T>> = computed(() => new MatTableDataSource<T>(this.curatedData()));
 
   //TODO: include filters
   itemCount = computed(() => this.curatedData.length);
